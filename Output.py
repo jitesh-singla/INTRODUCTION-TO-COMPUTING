ee29e7b220fd91193b1df81134a08485ff4971f4
jitesh-singla <97080361+jitesh-singla@users.noreply.github.com>
2022-01-13 14:44:55 +0530

    Add files via upload

---
+++ Output.py
@@ -0,0 +1,47 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: D:/html/assignment_1_Jitesh_Singla_21105068_ECE.py =========
+Question  1
+Please enter the first  no.-   16
+Please enter the second no.-   18
+Please enter the third  no.-   19
+17.666666666666668
+
+
+Question  2
+Enter your gross income - $150000
+Total no. of Dependents - 5
+Your tax = $ 25000.0
+
+
+Question  3
+SID-1698
+Name-Ram
+Gender(F,M or U)-M
+Course Name-ece
+CGPA-8.9
+[1698, 'Ram', 'M', 'ECE', 8.9]
+
+
+Question  4
+Plz give marks of the students 
+
+89
+78
+42
+59
+36
+--------Output---------
+36
+42
+59
+78
+89
+
+
+Question 5
+Part a
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+Part b
+['Red', 'Green', 'White', 'Purple', 'Yellow']
