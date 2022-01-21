db5d55ca234a71de0ae6b51ddec385b29bf40209
jitesh-singla <97080361+jitesh-singla@users.noreply.github.com>
2022-01-21 17:05:49 +0530

    Add files via upload

---
+++ Output_assign_2.py
@@ -0,0 +1,50 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: D:\coding\Python\assignment_2_Jitesh_Singla_21105068_ECE.py.py ===
+Question 1
+Part 1.a)
+35
+Part 1.b)
+egaugnal evitisnes esac a si nohtyP
+Part 1.c)
+The new string : a case sensitive
+Part 1.d)
+Python is object oriented language
+Part 1.e)
+10
+Part 1.f)
+Pythonisacasesensitivelanguage
+
+
+Question 2
+Hey, Jitesh Singla Here ! 
+My sid is 21105068 
+I am from ECE department and my CGPA is 9.800000
+
+
+Question 3
+a&b : 8
+a|b : 58
+a^b : 50
+a<<2 : 224 	b<<2 : 40
+a>>2 : 14 	b>>4 :  0
+
+
+Question 4
+enter the first no. :40
+enter the second no. :66
+enter the third no. :12
+Highest no. is : 66
+
+
+Question 5
+Enter a string :hlo world 
+No
+
+
+Question 6
+Enter 1st length :5
+Enter 2nd length :4
+Enter 3rd length :3
+Yes
