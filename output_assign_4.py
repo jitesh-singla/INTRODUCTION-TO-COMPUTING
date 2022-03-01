4b2479ba4d4055248887e8b7427c12b2d3db8f5d
jitesh-singla <97080361+jitesh-singla@users.noreply.github.com>
2022-03-01 22:10:56 +0530

    Add files via upload

---
+++ output_assign_4.py
@@ -0,0 +1,78 @@
+PS D:\coding\Python> python -u "d:\coding\Python\assign_04_21105068_ECE.py"
+############################### Question 1 ###############################
+
+Move disk 1 from source S to destination D
+move disk 2 from source S to destination A
+Move disk 1 from source D to destination A
+move disk 3 from source S to destination D
+Move disk 1 from source A to destination S
+move disk 2 from source A to destination D
+Move disk 1 from source S to destination D
+
+############################### Question 2 ###############################
+
+enter the no. of rows for passcal triangle :5
+with recursion 
+
+    1
+   1 1
+  1 2 1
+ 1 3 3 1
+1 4 6 4 1
+
+Without recursion
+
+    1
+   1 1
+  1 2 1
+ 1 3 3 1 
+1 4 6 4 1
+############################### Question 3 ###############################
+
+enter first no. :15
+enter secod no. :2
+(7, 1)
+
+Part a)
+False
+
+Part b)
+both values are non zero
+
+Part c)
+[7, 1, 4, 5, 6]
+7 5 6
+
+Part d)
+{5, 6, 7}
+
+Part e)
+the immutable set is : frozenset({5, 6, 7})
+
+Part f)
+max value : 7
+Hash value : 7
+
+############################### Question 4 ###############################
+
+Jitesh Singla
+21105068
+Record of  Jitesh Singla  destroyed
+
+############################### Question 5 ###############################
+
+name : Mehak salary : 40000
+name : Ashok salary : 50000
+name : Viren salary : 60000
+
+Part a
+updated salary of Mehak is 70000
+
+Part b
+Record of viren is deleted
+############################### Question 6 ###############################
+
+Give a word to test your friendship :fast
+Give a meaningfull word containing same letter of the word given by your friend :saft
+does the word entered by 2nd friend seems meaningfull ? (y/n) :y
+Test passed , friendship is true
